23f9d053d98ba048edfda62f8b1fc126cae611d2
yondav <yonidavid.abr@gmail.com>
2021-02-28 23:38:49 -0500

    created for loop to create 9 of each element and append them in the right layout. 9 rows appended

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,16 @@
 var currentDate = moment().format("ddd MMMM DD" + ", " + "YYYY");
 
+
+
+$(document).ready(function() {
 // Display today's date
 $("#currentDay").text(currentDate);
+
+// For loop to structure and append elements in the planner
+for (i = 0; i < 9; i++) {
+    var row = $("<div>").addClass("row");
+
+
+    $(".container").append(row);
+}
+});
