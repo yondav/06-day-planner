73a28d7d8ef80b12a2844a9c6f6adbc12a1b1884
yondav <yonidavid.abr@gmail.com>
2021-02-28 23:53:12 -0500

    Got the hours working! Usd col-md-12 class for now.

--- Develop/script.js
+++ Develop/script.js
@@ -2,6 +2,7 @@ var currentDate = moment().format("ddd MMMM DD" + ", " + "YYYY");
 
 
 
+
 $(document).ready(function() {
 // Display today's date
 $("#currentDay").text(currentDate);
@@ -9,8 +10,10 @@ $("#currentDay").text(currentDate);
 // For loop to structure and append elements in the planner
 for (i = 0; i < 9; i++) {
     var row = $("<div>").addClass("row");
-
+    var time = $("<div>").addClass("hour col-md-2").text(moment("9:00 AM", "hh:mm A").add(i, "hours").format("hA"));
+    time.attr("data-time", moment("9:00 AM", "hh:mm A").add(i, "hours").format("hA"));
 
     $(".container").append(row);
+    $(row).append(time);
 }
 });
