0110c0ee4c31660a4e86be182c51503fab995a12
yondav <yonidavid.abr@gmail.com>
2021-03-01 00:00:39 -0500

    appended textarea but layout looks wrong.

--- Develop/script.js
+++ Develop/script.js
@@ -13,12 +13,13 @@ for (i = 0; i < 9; i++) {
     var row = $("<div>").addClass("row");
     var time = $("<div>").addClass("hour col-md-2").text(moment("9:00 AM", "hh:mm A").add(i, "hours").format("hA"));
     // adding data attribute to time here to take on the index number. Will need to track the time for color coding later
-    time.attr("data-time", moment("9:00 AM", "hh:mm A").add(i, "hours").format("hA"));
-    
+    // time.attr("data-time", moment("9:00 AM", "hh:mm A").add(i, "hours").format("hA"));
+    var task = $("<textarea>");
 
 
     $(".container").append(row);
     $(row).append(time);
+    $(time).after(task);
 
 }
 });
