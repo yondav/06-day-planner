e60bf89a7442fbc6ebc8979e84fee2d104256618
yondav <yonidavid.abr@gmail.com>
2021-03-01 00:14:00 -0500

    appended all the elements in the layout. Messing with classes to get it right. Might be slightly off still but I think it looks good.

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -14,12 +14,13 @@ for (i = 0; i < 9; i++) {
     var time = $("<div>").addClass("hour col-md-2").text(moment("9:00 AM", "hh:mm A").add(i, "hours").format("hA"));
     // adding data attribute to time here to take on the index number. Will need to track the time for color coding later
     // time.attr("data-time", moment("9:00 AM", "hh:mm A").add(i, "hours").format("hA"));
-    var task = $("<textarea>");
+    var task = $("<textarea>").addClass("col-md-8");
+    var saveBtn = $("<button>").addClass("saveBtn col-md-2").html('<i class="fas fa-save"></i>');
 
 
     $(".container").append(row);
     $(row).append(time);
     $(time).after(task);
-
+    $(task).after(saveBtn);
 }
 });
